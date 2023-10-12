7d90b05ddf8a01fd1e2d1ac3a7a757a5dd60b0ee
Mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2023-10-12 21:58:44 +0300

    preparation of the project files

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cons = require('consolidate')
+const dust = require('dustjs-helpers')
+const pg = require('pg')
+const app = express();
+
+//connection string
+const connect = 'postgresql://MU-SALEH:123123@localhost/recipebookdb';
+
+// Assign Dust Engine to .dust files
+app.engine('dust',cons.dust)
+
+// Set Default Ext .dust 
+app.set('view engine','dust')
+app.set('views',__dirname+'/views')
+
+//set Public Folder 
+app.use(express.static(path.join(__dirname,'public')));
+
+// body parser middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/',function (req,res) {
+    console.log("Test");
+})
+
+// server
+app.listen(3000,function(){
+    console.log("Server started ON Port 3000");
+})
